5ca290ad66dd394736877e0f095c63cd3ad58fb2
joaovictorpsantos <joaovictorsantoscpv@gmail.com>
2021-03-20 14:39:02 -0300

    feature: add sum hours per month

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,4 +1,19 @@
 defmodule ReportsHours.Parser do
+  @months %{
+    "1" => :janeiro,
+    "2" => :feveiro,
+    "3" => :marco,
+    "4" => :abril,
+    "5" => :maio,
+    "6" => :junho,
+    "7" => :julho,
+    "8" => :agosto,
+    "9" => :setembro,
+    "10" => :outubro,
+    "11" => :novembro,
+    "12" => :dezembro
+  }
+
   def parse_file(filename) do
     filename
     |> File.stream!()
@@ -10,6 +25,6 @@ defmodule ReportsHours.Parser do
     |> String.trim()
     |> String.split(",")
     |> List.update_at(1, &String.to_integer/1)
-    |> List.update_at(3, &String.to_integer/1)
+    |> List.update_at(3, fn month -> @months[month] end)
   end
 end
--- lib/reports_hours.ex
+++ lib/reports_hours.ex
@@ -9,8 +9,9 @@ defmodule ReportsHours do
 
   defp sum_values(line, report) do
     all_hours = sum_all_hours(line, report.all_hours)
+    hours_per_month = sum_hours_per_month(line, report.hours_per_month)
 
-    build_report(all_hours)
+    build_report(all_hours, hours_per_month)
   end
 
   defp sum_all_hours([name, hours, _day, _month, _year], all_hours) do
@@ -19,9 +20,19 @@ defmodule ReportsHours do
     Map.put(all_hours, name, hours + previous_hours)
   end
 
-  defp build_report(all_hours) do
+  defp sum_hours_per_month([name, hours, _day, month, _year], hours_per_month) do
+    previous_historic = Map.get(hours_per_month, name, %{})
+    previous_hours_month = Map.get(previous_historic, month, 0)
+
+    new_month = Map.put(previous_historic, month, hours + previous_hours_month)
+
+    Map.put(hours_per_month, name, new_month)
+  end
+
+  defp build_report(all_hours, hours_per_month) do
     %{
-      all_hours: all_hours
+      all_hours: all_hours,
+      hours_per_month: hours_per_month
     }
   end
 
